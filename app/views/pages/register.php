569c481a5ca81b30be865ab9b6361cf83722ed46
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-17 10:29:20 +0530

    users controller

---
+++ app/views/pages/register.php
@@ -0,0 +1,10 @@
+<?php require_once APPROOT.'/views/inc/header.php' ?>
+
+<div class="card text-center col-md-6 mx-auto mt-3">    
+  <div class="card-body">
+    <h2>Create an Account</h2>
+  </div>
+</div>
+
+
+<?php require_once APPROOT.'/views/inc/footer.php' ?>
